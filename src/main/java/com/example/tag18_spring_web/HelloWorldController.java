08714c122ae028155bb61cb2551a4facd6699b13
FIRST_NAME LAST_NAME <Skiber03@gmail.com>
2023-09-13 17:00:19 +0200

    added POST and GetMapping. Post does not work

--- src/main/java/com/example/tag18_spring_web/HelloWorldController.java
+++ src/main/java/com/example/tag18_spring_web/HelloWorldController.java
@@ -1,22 +1,29 @@
 package com.example.tag18_spring_web;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloWorldController {
-
     @GetMapping()
     public String getHelloWorld(){
         return "Hello, World";
     }
-
-
     @GetMapping("/{name}")
     public String helloWorld(@PathVariable String name) {
         return "Hello, " + name;
+    }
+    private List<Message> messages = new ArrayList<>();
+
+    @GetMapping("/messages")
+    public List<Message> getMessages() {
+        return messages;
+    }
+    @PostMapping("/messages")
+    public void addMessage(@RequestBody Message message) {
+        messages.add(message);
     }
 }
