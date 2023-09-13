9fc6e78a77300adf271a9dbf3668ecf7a1632891
FIRST_NAME LAST_NAME <Skiber03@gmail.com>
2023-09-13 16:13:17 +0200

    added HelloWorldController and tested with Postman

---
+++ src/main/java/com/example/tag18_spring_web/Tag18SpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.tag18_spring_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tag18SpringWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tag18SpringWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tag18_spring_web/Tag18SpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tag18_spring_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tag18SpringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
