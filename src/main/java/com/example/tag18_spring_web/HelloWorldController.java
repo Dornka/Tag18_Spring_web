f37c9685772c25888a4d40676f4a7d3d9bc6972d
FIRST_NAME LAST_NAME <Skiber03@gmail.com>
2023-09-13 16:25:52 +0200

    added Path Variable sand tested with Postman

--- src/main/java/com/example/tag18_spring_web/HelloWorldController.java
+++ src/main/java/com/example/tag18_spring_web/HelloWorldController.java
@@ -2,12 +2,20 @@ package com.example.tag18_spring_web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloWorldController {
 
-    @GetMapping("/hello/{name}")
+    @GetMapping()
+    public String getHelloWorld(){
+        return "Hello, World";
+    }
+
+
+    @GetMapping("/{name}")
     public String helloWorld(@PathVariable String name) {
         return "Hello, " + name;
     }
